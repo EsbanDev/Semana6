f6475b3ec444c447797263ba53b40b806c70532b
EsbanDev <u22233927@utp.edu.pe>
2025-05-02 09:45:17 -0500

    cambio db config mejor

--- crud-vendedores/config/db.js
+++ crud-vendedores/config/db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: "tramway.proxy.rlwy.net",
+  host: "trolley.proxy.rlwy.net",
   user: "root",
-  password: "kQFQPMPIkAojatyUTITqzDGeSfpWuYfg",
+  password: "IAstqeesyDNFMxsQhQFvFnNvUwvdqZeq",
   database: "railway",
-  port: 35910,
+  port: 29741,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
